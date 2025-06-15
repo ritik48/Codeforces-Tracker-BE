46ed49ce23e6aa41985c39a1f4eda881be3f94f7
Ritik <raj769417@gmail.com>
2025-06-15 19:51:22 +0530

    feat: function to add a user

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 import { User } from "./models/user.model";
+import { Student } from "./models/student.model";
 
-const connectDB = async () => {
-  await mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/tle_cf_management"
-  );
-
+const addAdmin = async () => {
   await User.deleteMany();
 
   const username = process.argv[2];
@@ -13,8 +10,29 @@ const connectDB = async () => {
   const role = process.argv[4] || "admin";
 
   await User.create({ username, password, role });
-
-  await mongoose.disconnect();
 };
 
-connectDB();
+const addUser = async () => {
+  await Student.deleteMany();
+
+  const cf_handle = process.argv[2];
+
+  await Student.create({ cf_handle });
+};
+
+const main = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost:27017/tle_cf_management"
+    );
+
+    // await addAdmin();
+    await addUser();
+  } catch (err) {
+    console.error("❌ Error:", err);
+  } finally {
+    await mongoose.disconnect();
+  }
+};
+
+main();
