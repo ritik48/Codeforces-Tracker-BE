c976b6c829c98661aa113594a70b753b5a823849
Ritik <raj769417@gmail.com>
2025-06-15 21:22:35 +0530

    refactor: renamed problem model to submission and some schema changes

---
+++ src/models/submission.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+import {
+  STUDENT_COLLECTION_NAME,
+  PROBLEM_COLLECTION_NAME,
+} from "./model-config";
+
+export interface SubmissionDocument extends Document {
+  student: Schema.Types.ObjectId;
+  submissionId: number;
+  contestId: number;
+  index: string;
+  name: string;
+  rating: number;
+  verdict: string;
+  creationTime: Date;
+}
+
+const submissionSchema = new Schema<SubmissionDocument>({
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: STUDENT_COLLECTION_NAME,
+    required: true,
+  },
+  submissionId: Number,
+  contestId: Number,
+  index: String,
+  name: String,
+  rating: Number,
+  verdict: String,
+  creationTime: Date,
+});
+
+export const Submission = mongoose.model<SubmissionDocument>(
+  PROBLEM_COLLECTION_NAME,
+  submissionSchema
+);
