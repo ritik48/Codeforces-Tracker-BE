481b98cf56f31ec86c7e21d625883579a0d12836
Ritik <raj769417@gmail.com>
2025-06-19 01:51:50 +0530

    feat: added routes to set-update cron

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,10 @@ import { CookieOptions, NextFunction, Request, Response } from "express";
 import { User } from "../models/user.model";
 import { ApiError } from "../utils/ApiError";
 import { generateToken } from "../utils/token-helpers";
+import { Setting } from "../models/setting.model";
+import { asyncHandler } from "../utils/AsyncHandler";
+import { startCronJob } from "../cron/cron-init";
+import cron from "node-cron";
 
 const cookieOptions: CookieOptions = {
   httpOnly: true,
@@ -9,45 +13,41 @@ const cookieOptions: CookieOptions = {
   sameSite: "none",
 };
 
-export const loginUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    throw new ApiError("Missing username or password", 400);
+export const loginUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ApiError("Missing username or password", 400);
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw new ApiError("Invalid username or password", 401);
+    }
+
+    const isValidPassword = await user.comparePassword(password);
+    if (!isValidPassword) {
+      throw new ApiError("Invalid username or password", 401);
+    }
+
+    const token = generateToken(user._id as string);
+
+    res
+      .status(200)
+      .cookie("token", token, cookieOptions)
+      .send({ success: true, message: "Login successful" });
   }
+);
 
-  const user = await User.findOne({ username });
-  if (!user) {
-    throw new ApiError("Invalid username or password", 401);
+export const getUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(200).json({ user: req.user, success: true });
+    } catch (error) {
+      next(error);
+    }
   }
-
-  const isValidPassword = await user.comparePassword(password);
-  if (!isValidPassword) {
-    throw new ApiError("Invalid username or password", 401);
-  }
-
-  const token = generateToken(user._id as string);
-
-  res
-    .status(200)
-    .cookie("token", token, cookieOptions)
-    .send({ success: true, message: "Login successful" });
-};
-
-export const getUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    res.status(200).json({ user: req.user, success: true });
-  } catch (error) {
-    next(error);
-  }
-};
+);
 
 export const logoutUser = async (
   req: Request,
@@ -56,3 +56,39 @@ export const logoutUser = async (
 ) => {
   res.clearCookie("token", cookieOptions).status(200).json({ success: true });
 };
+
+export const updateCron = asyncHandler(async (req, res) => {
+  const { cron_time } = req.body;
+
+  if (!cron_time) {
+    throw new ApiError("Cron time is required", 400);
+  }
+
+  if (!cron.validate(cron_time)) {
+    throw new ApiError("Invalid cron time", 400);
+  }
+  const setting = await Setting.findOne();
+
+  // update setting if it is present else create new entry
+  if (setting) {
+    setting.cron_time = cron_time;
+    await setting.save();
+  } else {
+    const new_setting = new Setting({ cron_time });
+    await new_setting.save();
+  }
+
+  await startCronJob();
+
+  res.status(200).json({ success: true, message: "Updated successfully" });
+});
+
+export const fetchCron = asyncHandler(async (req, res) => {
+  const setting = await Setting.findOne();
+
+  if (!setting) {
+    throw new ApiError("Cron time is not set", 400);
+  }
+
+  res.status(200).json({ success: true, cron_time: setting.cron_time });
+});
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -9,12 +9,20 @@ import { ApiError } from "../utils/ApiError";
 import { generateToken } from "../utils/token-helpers";
 import { isAuthenticated } from "../utils/middleware";
 import { asyncHandler } from "../utils/AsyncHandler";
-import { getUser, loginUser, logoutUser } from "../controllers/user.controller";
+import {
+  fetchCron,
+  getUser,
+  loginUser,
+  logoutUser,
+  updateCron,
+} from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.post("/login", asyncHandler(loginUser));
 router.post("/logout", asyncHandler(logoutUser));
 router.get("/user", isAuthenticated, asyncHandler(getUser));
+router.patch("/cron", isAuthenticated, asyncHandler(updateCron));
+router.get("/cron", isAuthenticated, asyncHandler(fetchCron));
 
 export { router as userRouter };
