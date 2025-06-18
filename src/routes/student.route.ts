0f0db25d8d1c9643c2a993668cc55084faa6489b
Ritik <raj769417@gmail.com>
2025-06-19 02:59:26 +0530

    fix: download route position

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -16,12 +16,12 @@ const router = express.Router();
 
 router.get("/", isAuthenticated, fetchAllStudents);
 router.post("/", isAuthenticated, createStudent);
+router.get("/download", isAuthenticated, downloadStudentData);
 router.get("/:id", isAuthenticated, fetchStudent);
 router.get("/:id/contest-history", isAuthenticated, fetchStudentContestHistory);
 router.get("/:id/submission-data", isAuthenticated, fetchStudentSubmissionData);
 router.patch("/:id/email", isAuthenticated, updateStudentEmail);
 router.patch("/:id", isAuthenticated, updateStudent);
 router.delete("/:id", isAuthenticated, deleteStudent);
-router.get("/download", isAuthenticated, downloadStudentData);
 
 export { router as studentRouter };
